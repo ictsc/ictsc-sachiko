781f108656ca988bffa8cafe6b03b8583d19bac5
K-shir0 <kakishiro2190394@gmail.com>
2021-06-16 13:47:21 +0900

    add flutter_hooksとhooks_riverpodを導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'app.dart';
 
 void main() {
-  runApp(App());
+  runApp(
+    ProviderScope(
+      child: App(),
+    ),
+  );
 }
