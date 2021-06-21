76d84c8aa6be03a264d9dbaba4ec86d166013edb
K-shir0 <kakishiro2190394@gmail.com>
2021-06-21 09:51:37 +0900

    docs 説明を追加

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ictsc_sachiko/ui/common/header.dart';
 
+/// アプリ全体で使われるヘッダー
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
