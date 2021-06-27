7f8e8ac0ae59a3023d88f10557ee4dfd4456e246
K-shir0 <kakishiro2190394@gmail.com>
2021-06-27 12:01:47 +0900

    add 登録ページのUIを作成 #1

--- lib/ui/sign_up_page.dart
+++ lib/ui/sign_up_page.dart
@@ -12,17 +12,58 @@ class SignUpPage extends HookWidget {
       body: Center(
           child: SizedBox(
         width: 512,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              '登録',
-              style: Theme.of(context)
-                  .textTheme
-                  .headline6!
-                  .copyWith(fontWeight: FontWeight.bold),
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  '登録',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '何らかの問題が発生しました。',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2!
+                      .copyWith(color: Theme.of(context).colorScheme.error),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(labelText: 'ユーザー名'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(labelText: 'パスワード'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Text('登録'),
+                      )),
+                ),
+              ),
+            ],
+          ),
         ),
       )),
     );
