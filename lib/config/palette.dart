9a20986df67fd462097c6b773b7f230c05cb297b
K-shir0 <kakishiro2190394@gmail.com>
2021-06-27 22:36:52 +0900

    add テーマを赤に変更 #9

---
+++ lib/config/palette.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const themeMaterialColor = MaterialColor(0xFFE6003A, {
+    50: Color(0xFFFCE0E7),
+    100: Color(0xFFF8B3C4),
+    200: Color(0xFFF3809D),
+    300: Color(0xFFEE4D75),
+    400: Color(0xFFEA2658),
+    500: Color(0xFFE6003A),
+    600: Color(0xFFE30034),
+    700: Color(0xFFDF002C),
+    800: Color(0xFFDB0025),
+    900: Color(0xFFD50018),
+  });
+
+  static const themeColor = Color(0xFFE6003A);
+}
