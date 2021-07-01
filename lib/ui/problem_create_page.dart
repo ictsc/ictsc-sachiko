6e3c480a93bde7587bd60d44725f6309fbbcbcab
K-shir0 <kakishiro2190394@gmail.com>
2021-07-01 15:49:20 +0900

    add 問題追加ページのUIを追加 #11

--- lib/ui/problem_create_page.dart
+++ lib/ui/problem_create_page.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:ictsc_sachiko/ui/common/editor_header.dart';
@@ -13,22 +11,6 @@ class ProblemCreatePage extends HookWidget {
   Widget build(BuildContext context) {
     final togglePreview = useState(false);
 
-    // return Scaffold(
-    //   appBar: AppBar(),
-    //   body: Column(
-    //     children: [
-    //       TextField(),
-    //       Row(
-    //         children: [
-    //           Card(
-    //             child: SizedBox(width: 512, child: TextField()),
-    //           ),
-    //         ],
-    //       ),
-    //     ],
-    //   ),
-    // );
-
     return Scaffold(
         appBar: EditorHeader(appBar: AppBar()),
         floatingActionButton: Container(
@@ -40,55 +22,178 @@ class ProblemCreatePage extends HookWidget {
             label: const Text('保存する'),
           ),
         ),
-        body: Center(
-          child: SizedBox(
-            width: 1024,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'タイトル',
+        body: SingleChildScrollView(
+          child: Center(
+            child: SizedBox(
+              width: 1024,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'タイトル',
+                      ),
+                      style: Theme.of(context).textTheme.headline6,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        width: 812,
-                        child: Card(
-                            child: Padding(
-                          padding: const EdgeInsets.only(
-                              top: 40, bottom: 48, left: 32, right: 32),
-                          child: editor(
-                              isEditor: togglePreview.value, context: context),
-                        )),
-                      ),
-                      Expanded(
-                        child: Center(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('Preview'),
-                              Switch(
-                                value: togglePreview.value,
-                                onChanged: (value) =>
-                                    togglePreview.value = value,
-                                // child: Text('toggle'),
-                              ),
-                            ],
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0, left: 8.0),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 128,
+                          child: TextField(
+                            decoration: InputDecoration(
+                                labelText: '問題コード',
+                                labelStyle:
+                                    Theme.of(context).textTheme.caption),
                           ),
                         ),
-                      )
-                    ],
+                        const Padding(padding: EdgeInsets.only(right: 8.0)),
+                        SizedBox(
+                          width: 128,
+                          child: TextField(
+                            decoration: InputDecoration(
+                                labelText: 'ポイント',
+                                labelStyle:
+                                    Theme.of(context).textTheme.caption),
+                          ),
+                        ),
+                        const Padding(padding: EdgeInsets.only(right: 8.0)),
+                        SizedBox(
+                          width: 128,
+                          child: TextField(
+                            decoration: InputDecoration(
+                                labelText: '解決基準ポイント',
+                                labelStyle:
+                                    Theme.of(context).textTheme.caption),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Card(
+                              child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 40, bottom: 48, left: 32, right: 32),
+                            child: editor(
+                                isEditor: togglePreview.value,
+                                context: context),
+                          )),
+                        ),
+                        const Padding(padding: EdgeInsets.only(left: 16.0)),
+                        SizedBox(
+                          width: 156,
+                          child: Center(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Padding(
+                                  padding:
+                                      EdgeInsets.only(top: 8.0, bottom: 8.0),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'プレビュー',
+                                      style:
+                                          Theme.of(context).textTheme.subtitle2,
+                                    ),
+                                    Switch(
+                                      value: togglePreview.value,
+                                      onChanged: (value) =>
+                                          togglePreview.value = value,
+                                      // child: Text('toggle'),
+                                    ),
+                                  ],
+                                ),
+                                const Padding(
+                                  padding:
+                                      EdgeInsets.only(top: 8.0, bottom: 8.0),
+                                  child: Divider(),
+                                ),
+                                PopupMenuButton(
+                                  itemBuilder: (BuildContext context) => [],
+                                  enabled: false,
+                                  child: Center(
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          '作者',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .subtitle2,
+                                        ),
+                                        const Icon(
+                                          Icons.settings_rounded,
+                                          size: 16,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0),
+                                  child: Text(
+                                    '末登録',
+                                    style: Theme.of(context).textTheme.caption,
+                                  ),
+                                ),
+                                const Padding(
+                                  padding:
+                                      EdgeInsets.only(top: 8.0, bottom: 8.0),
+                                  child: Divider(),
+                                ),
+                                PopupMenuButton(
+                                  itemBuilder: (BuildContext context) => [],
+                                  enabled: false,
+                                  child: Center(
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          '前提問題',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .subtitle2,
+                                        ),
+                                        const Icon(
+                                          Icons.settings_rounded,
+                                          size: 16,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0),
+                                  child: Text(
+                                    'なし',
+                                    style: Theme.of(context).textTheme.caption,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ));
@@ -129,5 +234,12 @@ class ProblemCreatePage extends HookWidget {
         ),
       ),
     );
+  }
+}
+
+class ProblemCreateSideMenu extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
