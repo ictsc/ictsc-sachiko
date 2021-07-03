e0475d5fd473117be06326a1324a1e4400b3ddd2
K-shir0 <kakishiro2190394@gmail.com>
2021-07-03 09:23:47 +0900

    fix userName -> nameに修正

--- lib/model/authentication/sign_in_request.dart
+++ lib/model/authentication/sign_in_request.dart
@@ -2,13 +2,12 @@ import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'sign_in_request.freezed.dart';
-
 part 'sign_in_request.g.dart';
 
 @freezed
 abstract class SignInRequest with _$SignInRequest {
   const factory SignInRequest({
-    @JsonKey(name: 'name') required String userName,
+    required String name,
     required String password,
   }) = _SignInRequest;
 
