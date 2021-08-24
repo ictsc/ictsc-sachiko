bba3eba5a684d31ea8b91e3c710bb13de3cc569a
K-shir0 <kakishiro2190394@gmail.com>
2021-08-24 22:39:32 +0900

    add パスワードフォームを分離

--- lib/ui/common/form/password_textform_field.dart
+++ lib/ui/common/form/password_textform_field.dart
@@ -2,24 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class PasswordTextFormField extends HookWidget {
+  final TextEditingController controller;
 
+  const PasswordTextFormField({required this.controller});
 
   @override
   Widget build(BuildContext context) {
     final isObscurePassword = useState(false);
 
     return TextFormField(
-      // obscureText: !state.isObscurePassword,
+      obscureText: !isObscurePassword.value,
       decoration: InputDecoration(
           labelText: 'パスワード',
-          // suffixIcon:
+          suffixIcon: IconButton(
+            onPressed: () {
+              isObscurePassword.value = !isObscurePassword.value;
+            },
+            icon: !isObscurePassword.value
+                ? const Icon(Icons.visibility_off, size: 18)
+                : const Icon(Icons.visibility, size: 18),
+          )
           // provider.suffixObscurePasswordIconButton
-      ),
-      // controller: provider.passwordController,
+          // TODO 登録ページで使い回す予定
+          // autovalidateMode: AutovalidateMode.onUserInteraction,
+          // validator: provider.passwordValidator(),
+          ),
+      controller: controller,
       // TODO 登録ページで使い回す予定
       // autovalidateMode: AutovalidateMode.onUserInteraction,
       // validator: provider.passwordValidator(),
     );
   }
-
 }
